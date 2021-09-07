14b48461aaf5331dca254939cd211074b3ed44b2
César Carabetta <carabetta87@gmail.com>
2021-09-06 23:49:38 -0300

    Agregado component NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="nav">
+                <div>Home</div>
+                <div>La empresa</div>
+                <div>Operaciones</div>
+                <div>Contacto</div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
