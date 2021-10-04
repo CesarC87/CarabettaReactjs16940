2f010c114b9a75807c33e306f5e0de8fcfd13ccc
César Carabetta <carabetta87@gmail.com>
2021-10-04 15:10:43 -0300

    Mejoras botones

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import "./ItemCount.css";
 
-const ItemCount = ({initialStock, count}) => {
-    
+const ItemCount = ({initialStock, count}) => {    
     
     const [stock, setStock] = useState(initialStock);
     const [counter, setCounter] = useState(count);    
@@ -30,12 +29,12 @@ const ItemCount = ({initialStock, count}) => {
     return (        
         <>
         <div className="masMenos">            
-            <button onClick={disminuir}>-</button>
+            <button onClick={disminuir} className="btnMenos">-</button>
             <p id="clicks">{counter}</p>
-            <button onClick={aumentar}>+</button>            
+            <button onClick={aumentar} className="btnMas">+</button>            
         </div>
         <div>
-            <button onClick={onAdd}>Agregar</button>
+            <button onClick={onAdd} className="btn-agregar">Agregar</button>
         </div>
         <div>
             <p>Stock Disponible: {stock} </p>
