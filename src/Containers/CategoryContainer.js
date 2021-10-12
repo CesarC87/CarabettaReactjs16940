777371e4f6fa5da1f54cd6170536322d97a7b0c3
César Carabetta <carabetta87@gmail.com>
2021-10-11 22:52:40 -0300

    Duda category container

--- src/Containers/CategoryContainer.js
+++ src/Containers/CategoryContainer.js
@@ -1,32 +1,57 @@
 import React from 'react'
 import { useParams } from 'react-router';
+import { getFirestore } from '../firebase';
 import ItemList from '../components/ItemList/ItemList';
 
 const CategoryContainer = () => {
     const [data, setData] = React.useState([]);
     const {category} = useParams();
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState(null);
 
     React.useEffect(()=> {
-        const url = "http://localhost:3001/products";
+        const db = getFirestore();
+        const productsCollection = db.collection('products').where("category", "==" , category);
 
-        fetch(url).then((response) => {
-            if (response.ok) {
-                return response.json();
-            }else{
-                throw response;
+        setLoading(true);
+        productsCollection
+        .get()
+        .then((querySnapshot) => {
+            console.log(querySnapshot);
+            if (querySnapshot.empty) {
+                console.log("No hay productos para mostrar")
+            } else {
+                setData(querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } )));
             }
         })
-        .then((data) => data.filter((product) => product.category === category))       
-        .then((data) => setData(data)) 
-        .catch((error) => console.log(`Error ${error.status}`))
-        
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
     }, [category]);
-
+    
     return (
         <>        
+        {loading && <p>Cargando...</p>}
+          {error && (
+            <p>
+                Ha habido un error: {error.status} {error.statusText}
+            </p>
+        )}
         <ItemList data={data}/>
         </>
     )
 }
 
 export default CategoryContainer
+
+// const url = "http://localhost:3001/products";
+
+// fetch(url).then((response) => {
+//     if (response.ok) {
+//         return response.json();
+//     }else{
+//         throw response;
+//     }
+// })
+// .then((data) => data.filter((product) => product.category === category))       
+// .then((data) => setData(data)) 
+// .catch((error) => console.log(`Error ${error.status}`))
